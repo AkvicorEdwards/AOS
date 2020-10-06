4cc037cf5e759b2410c1cfb35ee74e2590fa340f
Akvicor <akvicor@gmail.com>
2020-10-06 21:27:08 +0800

    get value from asmhead.nas

--- src/bootpack.c
+++ src/bootpack.c
@@ -6,6 +6,7 @@ void io_store_eflags(int eflags);
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void init_screen(char *vram, int x, int y);
 
 #define COL8_000000		0
 #define COL8_FF0000		1
@@ -27,43 +28,18 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 void HariMain(void) {
     char *vram;
 	int xsize, ysize;
+	short *binfo_scrnx, *binfo_scrny;
+	int *binfo_vram;
 
 	init_palette();
-	vram = (char *) 0xa0000;
-	xsize = 320;
-	ysize = 200;
+	binfo_scrnx = (short *) 0x0ff4; // asmhead.nas 中
+	binfo_scrny = (short *) 0x0ff6;
+	binfo_vram = (int *) 0x0ff8;
+	xsize = *binfo_scrnx;
+	ysize = *binfo_scrny;
+	vram = (char *) *binfo_vram;
 
-    // 设置底色 浅暗蓝
-	boxfill8(vram, xsize, COL8_008484,  0,         0,          xsize -  1, ysize - 29);
-	
-    // 设置1像素高度的线 亮灰
-    boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 28, xsize -  1, ysize - 28);
-	// 设置1像素高度的线 白
-    boxfill8(vram, xsize, COL8_FFFFFF,  0,         ysize - 27, xsize -  1, ysize - 27);
-    // 设置底部 task bar 亮灰
-	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 26, xsize -  1, ysize -  1);
-    
-    // 绘制左下按钮的顶部线条 白
-	boxfill8(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
-	// 绘制坐下按钮的左侧线条 白
-    boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
-	// 绘制左下按钮的底部线条 暗灰
-    boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
-	// 绘制左下按钮的右侧线条 暗灰
-    boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
-	// 绘制左下按钮的底部线条 黑
-    boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
-	// 绘制左下按钮的右侧线条 黑
-    boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
-    
-    // 绘制右下按钮的顶部线条 暗灰
-	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
-    // 绘制右下按钮的左侧线条 暗灰
-	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
-	// 绘制右下按钮的底部线条 白
-    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
-	// 绘制右下按钮的右侧线条 白
-    boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+	init_screen(vram, xsize, ysize);
 
     for (;;) {
         io_hlt();
@@ -122,4 +98,38 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 			vram[y * xsize + x] = c;
 	}
 	return;
+}
+
+void init_screen(char *vram, int x, int y) {
+    // 设置底色 浅暗蓝
+	boxfill8(vram, x, COL8_008484,  0,     0,      x -  1, y - 29);
+    // 设置1像素高度的线 亮灰
+    boxfill8(vram, x, COL8_C6C6C6,  0,     y - 28, x -  1, y - 28);
+	// 设置1像素高度的线 白
+    boxfill8(vram, x, COL8_FFFFFF,  0,     y - 27, x -  1, y - 27);
+    // 设置底部 task bar 亮灰
+	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 26, x -  1, y -  1);
+    
+    // 绘制左下按钮的顶部线条 白
+	boxfill8(vram, x, COL8_FFFFFF,  3,     y - 24, 59,     y - 24);
+	// 绘制坐下按钮的左侧线条 白
+    boxfill8(vram, x, COL8_FFFFFF,  2,     y - 24,  2,     y -  4);
+	// 绘制左下按钮的底部线条 暗灰
+    boxfill8(vram, x, COL8_848484,  3,     y -  4, 59,     y -  4);
+	// 绘制左下按钮的右侧线条 暗灰
+    boxfill8(vram, x, COL8_848484, 59,     y - 23, 59,     y -  5);
+	// 绘制左下按钮的底部线条 黑
+    boxfill8(vram, x, COL8_000000,  2,     y -  3, 59,     y -  3);
+	// 绘制左下按钮的右侧线条 黑
+    boxfill8(vram, x, COL8_000000,  2,     y -  3, 59,     y -  3);
+
+    // 绘制右下按钮的顶部线条 暗灰
+	boxfill8(vram, x, COL8_848484, x - 47, y - 24, x -  4, y - 24);
+    // 绘制右下按钮的左侧线条 暗灰
+	boxfill8(vram, x, COL8_848484, x - 47, y - 23, x - 47, y -  4);
+	// 绘制右下按钮的底部线条 白
+    boxfill8(vram, x, COL8_FFFFFF, x - 47, y -  3, x -  4, y -  3);
+	// 绘制右下按钮的右侧线条 白
+    boxfill8(vram, x, COL8_FFFFFF, x -  3, y - 24, x -  3, y -  3);
+    return;
 }
