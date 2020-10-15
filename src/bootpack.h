128e504a8dddae22647029a46cc34ae9550c22ef
Akvicor <akvicor@gmail.com>
2020-10-15 16:02:25 +0800

    keyboard input api

--- src/bootpack.h
+++ src/bootpack.h
@@ -234,10 +234,11 @@ void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, i
 void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
 void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
 
-/* console.c */
+// console.c
 struct CONSOLE {
 	struct SHEET *sht;
 	int cur_x, cur_y, cur_c;
+	struct TIMER *timer;
 };
 void console_task(struct SHEET *sheet, unsigned int memtotal);
 void cons_putchar(struct CONSOLE *cons, int chr, char move);
@@ -253,6 +254,7 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline);
 int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax);
 int *inthandler0d(int *esp);
 int *inthandler0c(int *esp);
+void hrb_api_linewin(struct SHEET *sht, int x0, int y0, int x1, int y1, int col);
 
 // file.c
 struct FILEINFO {
--- src/console.c
+++ src/console.c
@@ -3,7 +3,6 @@
 #include <string.h>
 
 void console_task(struct SHEET *sheet, unsigned int memtotal) {
-	struct TIMER *timer;
 	struct TASK *task = task_now();
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 	int i, fifobuf[128], *fat = (int *) memman_alloc_4k(memman, 4 * 2880);
@@ -16,9 +15,9 @@ void console_task(struct SHEET *sheet, unsigned int memtotal) {
 	*((int *) 0x0fec) = (int) &cons;
 
 	fifo32_init(&task->fifo, 128, fifobuf, task);
-	timer = timer_alloc();
-	timer_init(timer, &task->fifo, 1);
-	timer_settime(timer, 50);
+	cons.timer = timer_alloc();
+	timer_init(cons.timer, &task->fifo, 1);
+	timer_settime(cons.timer, 50);
 	file_readfat(fat, (unsigned char *) (ADR_DISKIMG + 0x000200));
 
 	//显示提示符
@@ -34,17 +33,17 @@ void console_task(struct SHEET *sheet, unsigned int memtotal) {
 			io_sti();
 			if (i <= 1) { //光标用定时器
 				if (i != 0) {
-					timer_init(timer, &task->fifo, 0); //下次置0
+					timer_init(cons.timer, &task->fifo, 0); //下次置0
 					if (cons.cur_c >= 0) {
 						cons.cur_c = COL8_FFFFFF;
 					}
 				} else {
-					timer_init(timer, &task->fifo, 1); //下次置1
+					timer_init(cons.timer, &task->fifo, 1); //下次置1
 					if (cons.cur_c >= 0) {
 						cons.cur_c = COL8_000000;
 					}
 				}
-				timer_settime(timer, 50);
+				timer_settime(cons.timer, 50);
 			}
 			if (i == 2) { // 光标ON
 				cons.cur_c = COL8_FFFFFF;
@@ -307,6 +306,7 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 		// 强行改写通过PUSHAD保存的值
 		// reg[0] : EDI,   reg[1] : ESI,   reg[2] : EBP,   reg[3] : ESP
 		// reg[4] : EBX,   reg[5] : EDX,   reg[6] : ECX,   reg[7] : EAX
+	int i;
 
 	if (edx == 1) {
 		cons_putchar(cons, eax & 0xff, 1);
@@ -362,6 +362,36 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 		}
 	} else if (edx == 14) {
 		sheet_free((struct SHEET *) ebx);
+	} else if (edx == 15) {
+		for (;;) {
+			io_cli();
+			if (fifo32_status(&task->fifo) == 0) {
+				if (eax != 0) {
+					task_sleep(task);	// FIFO 为空，休眠并等待
+				} else {
+					io_sti();
+					reg[7] = -1;
+					return 0;
+				}
+			}
+			i = fifo32_get(&task->fifo);
+			io_sti();
+			if (i <= 1) { // 光标用定时器
+				// 应用程序运行时不需要显示光标，因此总是将下次显示用的值置为1
+				timer_init(cons->timer, &task->fifo, 1); /* 次は1を */
+				timer_settime(cons->timer, 50);
+			}
+			if (i == 2) { // 光标ON
+				cons->cur_c = COL8_FFFFFF;
+			}
+			if (i == 3) { // 光标OFF
+				cons->cur_c = -1;
+			}
+			if (256 <= i && i <= 511) { // 键盘数据（通过任务A）
+				reg[7] = i - 256;
+				return 0;
+			}
+		}
 	}
 	return 0;
 }
